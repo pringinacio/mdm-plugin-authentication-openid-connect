4a9a10b8ea702f39608440364d0a840c61f1802a
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2022-04-28 16:24:04 +0100

    Update to J17.0.3 and G5.1.7 in line with mdm-core
    Also provide a standard way of making sure mdm-bom properties override all others

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/authentication/openid/connect/MdmPluginAuthenticationOpenidConnectGrailsPlugin.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/authentication/openid/connect/MdmPluginAuthenticationOpenidConnectGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import org.springframework.boot.web.servlet.ServletListenerRegistrationBean
 class MdmPluginAuthenticationOpenidConnectGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = '5.1.2 > *'
+    def grailsVersion = '5.1.7 > *'
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
